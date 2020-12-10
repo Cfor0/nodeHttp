1eada16f3433693ecbaba8aeeae01589c7911dba
cfor0 <cfor0@outlook.com>
2020-12-10 14:47:06 -0500

    added put route to update employee data

--- mediumHard/server.js
+++ mediumHard/server.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     if (!data) {
         res.status(404).send('Data not found')
     }
+    
     res.send(data)
 })
 
@@ -27,22 +28,47 @@ app.get('/employees/:id', (req, res) => {
 })
 
 app.post('/employees', (req, res) => {
-    if(!req.body.name || !req.body.salary || !req.body.department) {
+    if (!req.body.name || !req.body.salary || !req.body.department) {
         res.status(400).send('Bad Request, please make sure you fill out all the fields.')
     }
     const newEmployee = {
-        id: data.length +1,
+        id: data.length + 1,
         name: req.body.name,
         salary: req.body.salary,
         department: req.body.department
     }
 
     data.push(newEmployee)
- 
-    
 
     res.send(newEmployee)
 
+})
+
+app.put('/employees/:id', (req, res) => {
+    const updateEmployee = data.find((employee) => parseInt(req.params.id) === employee.id);
+
+    if (!updateEmployee) {
+        res.status(404).send('No employee found, please make sure you used the right id number')
+    }
+
+
+    if (req.body.name != undefined || req.body.name != null) {
+        updateEmployee.name = req.body.name;
+    } 
+    
+    if(req.body.salary != undefined || req.body.salary != null) {
+        updateEmployee.salary = req.body.salary;
+    }
+
+    if(req.body.department != undefined || req.body.department != null) {
+        updateEmployee.department = req.body.department;
+    } 
+   
+    // console.log( updateEmployee.name = req.body.name)
+    // console.log(updateEmployee.salary = req.body.salary);
+    // console.log(updateEmployee.department = req.body.department);
+
+    res.send(updateEmployee)
 })
 
 
